c86d0cd74fb6e74f9c50580e3228f6b3370b8915
liliia <liliia.allansson0085@stud.hkr.se>
2021-04-21 20:47:11 +0200

    moved xfml files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 
-import Models.Database;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,7 +11,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/View/logIn.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("logIn.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/logIn.fxml"));
         primaryStage.setTitle("ATM");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
