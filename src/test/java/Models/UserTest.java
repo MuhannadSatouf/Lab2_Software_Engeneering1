158f17a7ece390c5995efa30a7d48a7c0f4ead04
liliia <liliia.allansson0085@stud.hkr.se>
2021-04-21 11:53:36 +0200

    added unit test class UserTest

---
+++ src/test/java/Models/UserTest.java
@@ -0,0 +1,31 @@
+package Models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+    //User testUser = new User();
+
+
+    @Test
+    public void getName() {
+
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getPhoneNumber() {
+    }
+
+    @Test
+    public void getPassword() {
+    }
+}
